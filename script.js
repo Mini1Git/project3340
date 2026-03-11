395a8bcb758131a04b6e961177bd6df6742b5b16
nazifatahsinturna <nazifatahsinturnabd@gmail.com>
2026-03-11 16:24:03 -0400

    added the password eye rules

--- script.js
+++ script.js
@@ -13,4 +13,22 @@ hamMenu.addEventListener('click', () => {
     } else {
         hamImg.src = './icons/hamburger.svg';
     }
+});
+
+//rules for password in forms
+const eye = document.querySelector(".eye");
+let visible = false;
+
+eye.addEventListener('click', ()=>{
+    eye.classList.toggle("active");
+    const pass = document.querySelector('.password');
+    if(visible===false) {
+        pass.type="text";
+        visible = true;
+    }
+    else{
+        pass.type="password";
+        visible = false;
+    }
+
 })
