83fe443e47ab8e85109a5d7ae405d35d98d86d9d
Kayden <62318961+Marvoos@users.noreply.github.com>
2026-03-14 18:01:03 -0400

    Fixed a bug
    Prevented the user from accessing the menu.

--- scripts/restaurantScript.js
+++ scripts/restaurantScript.js
@@ -9,7 +9,7 @@ async function fetchRestaurant(vendors){ //it'a json for now
         json.forEach(restaurant =>{
             const listitem = document.createElement("li");
             const anchor = document.createElement("a");
-            anchor.href = "menu.html"; //adding it temporariliy like this
+            anchor.href = "../services/menu.html"; //adding it temporariliy like this
             
             const image = document.createElement("img");
             image.src = restaurant["img_path"];
