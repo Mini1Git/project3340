fc28eee7c4bb93ab94084d3248a162686325e946
nazifatahsinturna <nazifatahsinturnabd@gmail.com>
2026-03-25 16:43:59 -0400

    some cart stuff

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,2 +1,13 @@
 const cart = sessionStorage.getItem('cart') || [];
 console.log(cart);
+let total = sessionStorage.getItem('total') || 0; 
+if(cart.length === 0){
+    //if cart is empty
+}
+else{
+    create_cart()
+}
+
+function create_cart(){
+
+}
