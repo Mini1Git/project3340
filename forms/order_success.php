23789188786568eaab3050d618cf03f307780750
bell23332 <bell23@uwindsor.ca>
2026-04-03 17:37:52 -0400

    message when placed an order

---
+++ forms/order_success.php
@@ -0,0 +1,36 @@
+<?php
+    session_start();
+    // check to make sure they are logged in
+    $isLoggedIn = isset($_SESSION['user_id']);
+    if (!$isLoggedIn) {
+        header("Location: login.php");
+        exit();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Order Placed</title>
+    <link rel="stylesheet" href="../stylesheets/style.css">
+    <link rel="stylesheet" href="../stylesheets/stylealternate.css">
+    <link rel="stylesheet" href="../stylesheets/receiptStyle.css">
+</head>
+<body class="receipt-body">
+    <div class="receipt-container">
+        <header class="receipt-header">
+            <i class="fa-solid fa-paper-plane"></i>
+            <br>
+            <h1>Order Placed</h1>
+        </header>
+
+        <h3>
+            Thank you for ordering from Grillow! Your order will be arriving shortly!
+        </h3>
+        <div class="receipt-footer">
+            <a href="../services/orders.php" class="back-link">See Your Orders</a>
+        </div>
+    </div>
+</body>
+</html>
